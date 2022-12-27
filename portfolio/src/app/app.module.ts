ba53f6657bf9f7f174170b8ef9c8366401e66f04
caaylow <jupbiondo@gmail.com>
2022-12-27 19:43:55 -0300

    Dynamic navbar with navItems components iterable

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { NavBarComponent } from './components/navbar/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { NavBrandComponent } from './components/nav-brand/nav-brand.component';
+import { NavBrandComponent } from './components/navbar/nav-brand/nav-brand.component';
 import { IntroSectionComponent } from './components/intro-section/intro-section.component';
 import { SkillsSectionComponent } from './components/skills-section/skills-section.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
-import { NavItemComponent } from './components/nav-item/nav-item.component';
+import { NavItemComponent } from './components/navbar/nav-item/nav-item.component';
 
 
 @NgModule({
--- portfolio/src/app/components/navbar/nav-item/nav-item.component.ts
+++ portfolio/src/app/components/navbar/nav-item/nav-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-nav-item',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-item.component.css']
 })
 export class NavItemComponent {
-
+  @Input() navItemName!:string;
 }
