dfc2bc947b7e9b14c9294d5bdf702e8cf146c9e2
Juan Pablo <jupbiondo@gmail.com>
2023-01-02 15:25:42 -0300

    Add ContactFormComponent to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule, OnInit } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { MainComponent } from './components/main/main.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 
 const routes: Routes = [
   {path: "", component:MainComponent},
   {path:"acerca-de", component: AboutMeComponent},
+  {path: "contacto", component: ContactFormComponent},
 ];
 
 @NgModule({
