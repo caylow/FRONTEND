8108d2608e6c373f9a3e2383ec6cb883391fe61a
Juan Pablo <jupbiondo@gmail.com>
2023-01-02 15:23:41 -0300

    Remove NavItem component.
    Remove NavItem component, since it's not necessary for the project to have a separated component for each nav item till now.
    Also move NavBarComponent to the root Component folder.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './components/navbar/nav-bar/nav-bar.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
 import { SkillsSectionComponent } from './components/skills-section/skills-section.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
-import { NavItemComponent } from './components/navbar/nav-item/nav-item.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { CardComponentComponent } from './components/card-component/card-component.component';
 
@@ -20,7 +19,6 @@ import { CardComponentComponent } from './components/card-component/card-compone
     MainComponent,
     SkillsSectionComponent,
     ContactFormComponent,
-    NavItemComponent,
     AboutMeComponent,
     CardComponentComponent,
   ],
