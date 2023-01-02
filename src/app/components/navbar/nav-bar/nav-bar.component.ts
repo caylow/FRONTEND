8d96462329cb7d7750d7bce11f48c3a57a93acd1
Juan Pablo <jupbiondo@gmail.com>
2023-01-02 15:14:15 -0300

    Show active route in navbar
    Using routerLinkActive in nav-bar component I add a class called "active" whenever the routerlink matches the path. I use the 'routerLinkActiveOptions' directive to use the 'exact' option so that the rootLink '/' won't be always active.

--- src/app/components/navbar/nav-bar/nav-bar.component.ts
+++ src/app/components/navbar/nav-bar/nav-bar.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent{
   @Input() brandname!:string;
   @Input() navItems!:string[][];
 }
